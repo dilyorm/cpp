718ceaf70e16fed436208fe26f54509c36a71c79
Dilyorbekm <d.muhammadjonov@newuu.uz>
2025-02-20 12:29:46 +0500

    lab 6

--- main.cpp
+++ main.cpp
@@ -1,289 +1,294 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int p1_1() {
-    for (int i = 1; i <= 10; ++i) {
-        cout << i<<" ";
+    int sum=0;
+    for (int i = 1; i <= 100; i++) {
+        sum=sum+i;
     }
+    cout<<sum<<endl;
 }
 
 int p1_2() {
-    int N;
+    int N,sum=0;
     cin >> N;
-    for (int i = 1; i <= N; ++i) {
-        cout << i<<" ";
+    for (int i = 1; i <= N; i++) {
+        sum=sum+i;
     }
+    cout<<sum<<endl;
 }
 
-int p1_3() {
-    int sum = 0;
-    for (int i = 1; i <= 10; ++i) {
-        sum += i;
-    }
-    cout << sum;
-}
-
-int p1_4() {
-    int N, sum = 0;
-    cin >> N;
-    for (int i = 1; i <= N; ++i) {
-        sum += i;
-    }
-    cout << sum;
-}
-
-int p1_5() {
-    int sum = 0;
-    for (int i = 1; i <= 10; ++i) {
-        sum += i;
-    }
-    cout << sum / 10.0;
-}
-
-int p1_6() {
-    int N, sum = 0;
-    cin >> N;
-    for (int i = 1; i <= N; ++i) {
-        sum += i;
-    }
-    cout << sum / float(N);
-}
 
 int p2_1() {
-    for (int i = 1; i <= 10; i += 2) {
-        cout << i << " ";
+    int prime=0,in=2,flag=0;
+    while (prime<10) {
+        for (int i=2;i<=sqrt(in)+1;i++) {
+            if (in%i==0 and in!=2) {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==0) {
+            cout<<in<<endl;
+            prime++;
+        }
+        in++;
+        flag=0;
     }
 }
 
 int p2_2() {
-    for (int i = 2; i <= 10; i += 2) {
-        cout << i << " ";
-    }
-}
-
-int p2_3() {
-    int N;
-    cin >> N;
-    for (int i = 1; i <= N; i += 2) {
-        cout << i << " ";
-    }
-}
-
-int p2_4() {
-    int N;
-    cin >> N;
-    for (int i = 2; i <= N; i += 2) {
-        cout << i << " ";
-    }
-}
-
-int p2_5() {
-    for (int i = 10; i <= 100; i += 10) {
-        cout << i << " ";
-    }
-}
-
-int p2_6() {
-    int N;
-    cin >> N;
-    for (int i = 10; i <= N; i += 10) {
-        cout << i << " ";
+    int n,prime=0,in=2,flag=0;
+    cin>>n;
+    while (prime<n) {
+        for (int i=2;i<=sqrt(in)+1;i++) {
+            if (in%i==0 and in!=2) {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==0) {
+            cout<<in<<endl;
+            prime++;
+        }
+        in++;
+        flag=0;
     }
 }
 
 int p3() {
-    for (char c = 'A'; c <= 'Z'; ++c) {
-        cout << c << " ";
-        if ((c - 'A' + 1) % 5 == 0) {
-            cout << endl;
+    int n;
+    cin>>n;
+    while (n>1) {
+        cout<<n<<",";
+        if (n%2==0) {
+            n=n/2;
+        }
+        else {
+            n=n*3+1;
         }
     }
+    cout<<1<<endl;
 }
 
 int p4() {
-    int n, fact = 1;
-    cout << "Enter a number: ";
-    cin >> n;
-    if (n < 0) {
-        cout << "Factorial of a negative number is not defined.";
-    } else {
-        for (int i = 1; i <= n; ++i) {
-            fact *= i;
-        }
-        cout << "Factorial of " << n << " is " << fact;
+    int n,count=1;
+    cin>>n;
+    while (n>1) {
+        count+=1;
+        n=n/10;
     }
+    cout<<count<<endl;
 }
 
 int p5() {
-    int num_classes;
-    float total_GPA = 0.0, total_credits = 0.0;
-
-    cout << "Enter the number of classes: ";
-    cin >> num_classes;
-
-    for (int i = 0; i < num_classes; ++i) {
-        int credits;
-        float marks;
-        cout << "Enter credits and marks for class " << (i + 1) << ": ";
-        cin >> credits >> marks;
-
-        total_GPA += credits * marks;
-        total_credits += credits;
+    int n;
+    cin>>n;
+    while (n>1) {
+        cout<<n%10<<" ";
+        n=n/10;
     }
-
-    cout << "Your total GPA is " << total_GPA / total_credits;
+    cout<<n<<endl;
 }
-
+int findGCD(int a, int b) {
+    if (a == 0)
+        return b;
+    return findGCD(b % a, a);
+}
 int p6() {
-    int num_students, num_classes;
-    float total_GPA = 0.0, total_credits = 0.0;
-
-    cout << "Enter number of students: ";
-    cin >> num_students;
-
-    for (int i = 0; i < num_students; ++i) {
-        cout << "Enter number of classes for student " << (i + 1) << ": ";
-        cin >> num_classes;
-
-        for (int j = 0; j < num_classes; ++j) {
-            int credits;
-            float marks;
-            cout << "Enter credits and marks for class " << (j + 1) << ": ";
-            cin >> credits >> marks;
-
-            total_GPA += credits * marks;
-            total_credits += credits;
-        }
-    }
-
-    cout << "The total GPA of the class is " << total_GPA / total_credits;
+    int a,b;
+    cin>>a>>b;
+    cout<<findGCD(a,b);
 }
 
 int p7() {
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << "*";
-        }
-        cout << endl;
+    int sum=0,counter=0,n;
+    while (sum<100) {
+        cout<<"Enter a number: ";
+        cin>>n;
+        sum=sum+n;
+        counter++;
     }
+    cout<<"Sum exceeded 100! Total sum: "<<sum<<endl<<"Total numbers entered: "<<counter;
 }
 
 int p8() {
-    int n;
-    cin >> n;
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
+    int n,balance=500;
+    cout<<"Your balance: $500";
+    while (balance>0) {
+        cout<<"Enter withdrawal amount (or 0 to cancel): ";
+        cin >> n;
+        if (balance<n) {
+            cout<<"Insufficient funds!"<<endl;
         }
-        cout << endl;
-    }
+        else {
+            balance=balance-n;
+            cout<<"Remaining balance: $"<<balance<<endl;
+        }}
 }
 
 int p9() {
-    int num;
-    cin >> num;
-
-    bool prime = true;
-    if (num <= 1) {
-        prime = false;
+    string k;
+    cout<<"Enter N or Y: ";
+    cin>>k;
+    while (k!="N" and k!="Y") {
+        cout<<"Invalid"<<endl;
+        cin>>k;
     }
-
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            prime = false;
-            break;
-        }
-    }
-
-    if (prime) {
-        cout << "Number is prime";
-    } else {
-        cout << "Number is not prime";
-    }
+    cout<<"Accepted";
 }
 
 int p10() {
-    int n;
-    cin >> n;
-
-    for (int i = 1; i <= n; ++i) {
-        cout << i << "*" << i << "=" << (i * i) << endl;
+    string user_input;
+    cin>>user_input;
+    while (user_input!="Exit") {
+        cout<<"Processing..."<<endl;
+        cin>>user_input;
     }
+    cout<<"Goodbye!"<<endl;
+}
+double add(double x, double y) {
+    return x + y;
 }
 
-int p11() {
-    int num, pos = 0, neg = 0, sum = 0, count = 0;
-    while (true) {
-        cout << "Enter an integer,the input ends if it is 0: ";
-        cin >> num;
+double subtract(double x, double y) {
+    return x - y;
+}
 
-        if (num == 0) break;
+double multiply(double x, double y) {
+    return x * y;
+}
 
-        if (num > 0) {
-            ++pos;
-        } else {
-            ++neg;
-        }
-        sum += num;
-        ++count;
+double divide(double x, double y) {
+    if (y == 0) {
+        cout << "Error! Division by zero." << endl;
+        return 0;
     }
+    return x / y;
+}
+void menu() {
+    cout << "Menu:" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Exit" << endl;
+}
+int p11() {
+    double num1, num2;
+    int choice;
 
-    if (count != 0) {
-        cout << "The number of positives is " << pos << endl;
-        cout << "The number of negatives is " << neg << endl;
-        cout << "The total is " << sum << endl;
-        cout << "The average is " << float(sum) / count << endl;
-    } else {
-        cout << "No numbers entered.";
+    while (true) {
+        menu(); // Display the menu
+        cout << "Enter choice (1/2/3/4/5): ";
+        cin >> choice;
+
+        if (choice == 5) {
+            cout << "Exiting the calculator. Goodbye!" << endl;
+            break;
+        }
+
+        if (choice >= 1 && choice <= 4) {
+            cout << "Enter first number: ";
+            cin >> num1;
+            cout << "Enter second number: ";
+            cin >> num2;
+
+            switch (choice) {
+                case 1:
+                    cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
+                break;
+                case 2:
+                    cout << num1 << " - " << num2 << " = " << subtract(num1, num2) << endl;
+                break;
+                case 3:
+                    cout << num1 << " * " << num2 << " = " << multiply(num1, num2) << endl;
+                break;
+                case 4:
+                    cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
+                break;
+            }
+        } else {
+            cout << "Invalid choice! Please select a valid option." << endl;
+        }
     }
 }
 
 int p12() {
     int num;
+    cout<<"Enter password: ";
     cin >> num;
 
-    while (num != 0) {
-        cout << num % 10;
-        num /= 10;
+    while (num != 6543) {
+        cout<<"Incorrect"<<endl;
+        cout<<"Enter password: "<<endl;
+        cin >> num;
     }
+    cout<<"Access Granted";
 }
 int p13() {
-    int n;
+    long long int n,a;
+    cout<<"Guess: ";
     cin >> n;
+    srand(time(0));
 
-    if (n == 0) {
-        cout << "The depth is 0";
-    } else {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= i; ++j) {
-                cout << i;
-            }
-            cout << endl;
+    // Generate a random number between 0 and RAND_MAX
+    a = rand() % 100 + 1;
+    cout<<a<<endl;
+    while (n!=a) {
+        if (n<a) {
+            cout<<"too low"<<endl;
+        }
+        else if (n>a) {
+            cout<<"too high"<<endl;
+        }
+        cout<<"Guess: ";
+        cin >> n;
+    }
+    cout<<"Correct!"<<endl;
+}
+
+//p14
+void welcomeMessage() {
+    cout<<"Welcome to the C++ programming world!";
+}
+//p15
+void greetUser() {
+    string user_input;
+    cout<<"Enter your name: ";
+    cin>>user_input;
+    cout <<"Hello,"<< user_input <<"!"<< endl;
+}
+//p16
+int maxNumber(int a, int b) {
+    if (a>=b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+//p17
+int factorial(int n) {
+    int f=1;
+    for (int i = 1; i<=n; i++) {
+        f=f*i;
+    }
+    return f;
+}
+//p18
+int printRectangle(int width, int height) {
+    cout<<"The rectangle is :"<<endl;
+    for (int i = 1; i<=height; i++) {
+        cout<<endl;
+        for (int j = 1; j<=width; j++) {
+            cout<<"*";
         }
     }
 }
-
-int p14() {
-    string binary;
-    cin >> binary;
-
-    int decimal = 0;
-    int length = binary.size();
-
-    for (int i = 0; i < length; ++i) {
-        if (binary[length - 1 - i] == '1') {
-            decimal += pow(2, i);
-        }
-    }
-
-    cout << "The decimal number is " << decimal;
-}
-
 int main() {
-    p14();
+    printRectangle(3,2);
 }
